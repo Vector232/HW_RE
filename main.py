bdf248b56bf5a7f727bafe39a5dcf3aded0ce8d1
Vlad Troyan <030705vt@mail.ru>
2023-07-29 14:18:48 +0300

    realy finish commit

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ with open("phonebook_raw.csv", encoding='utf-8') as f:
 
 ## 1. Выполните пункты 1-3 задания.
 phonebook = phonebook_fixer(contacts_list)
+phonebook.fix()
+phonebook_list = phonebook.get_list()
 
 ## 2. Сохраните получившиеся данные в другой файл.
 ## Код для записи файла в формате CSV:
@@ -15,4 +17,4 @@ with open("phonebook.csv", "w", encoding='utf-8') as f:
   datawriter = csv.writer(f, delimiter=',')
   
 ## Вместо contacts_list подставьте свой список:
-  datawriter.writerows(phonebook)
+  datawriter.writerows(phonebook_list)
--- module.py
+++ module.py
@@ -1,18 +1,72 @@
-def name_fixer(last, first, surname):
-    def disconnector(string):
-        pass
-    def relocator(first, second, third):
-        pass
-    pass
+import re
 
-def phones_fixer(phone):
-    pass
 
-def coupler(first, second):
-    pass
+class phonebook_fixer:
+    def __init__(self, phonebook_raw) -> None:
+        self.phonebook_raw = phonebook_raw
+        self.phonebook = [phonebook_raw[0]]
 
-def phonebook_fixer(phonebook_raw):
-    phonebook = ['N', phonebook_raw[0]]
-    for n, note in enumerate(phonebook_raw[1:]):
-        phonebook.append([n, note])
-    return phonebook
+    def __str__(self, phonebook) -> str:
+        output = ''
+        for note in phonebook:
+            output = output + note
+        return output
+    
+    def get_list(self):
+        return self.phonebook
+
+    # основа алгоритма
+    def fix(self):
+        temp_phonebook = []
+        # собирает данные из двух записей в одну
+        def coupler(note1, note2):
+            for i, val in enumerate(note1):
+                if val == '':
+                    note1[i] = note2[i]
+            return note1
+
+        # приводит в порядок ФИО
+        def name_fixer(last, first, surname):
+            lastname_list = last.split(sep=" ")
+            firstname_list = first.split(sep=" ")
+
+            if len(lastname_list) == 3:
+                last, first, surname = lastname_list
+            elif len(lastname_list) == 2:
+                last, first = lastname_list
+            elif len(firstname_list) == 2:
+                first, surname = firstname_list
+
+            return [last, first, surname]
+
+        # приводит телефонные номера к одному стандарту
+        def phones_fixer(phone):
+            patern = r'(\+7|8)?\s*\(?(\d{3})\)?[-\s]*(\d{0,3})[-\s]*(\d{2})[-\s]*(\d{2,4})(\s*\(?доб\.\s*(\d{4})\)?)?'
+            replace_add = r'+7(\2)\3-\4-\5 доб.\7'
+            replace = r'+7(\2)\3-\4-\5'
+
+            if re.search(r'доб', phone) != None:
+                standart_phone = re.sub(patern, replace_add, phone)
+            else:
+                standart_phone = re.sub(patern, replace, phone)
+
+            return standart_phone
+        
+        # приводим в порядок данные
+        for note in self.phonebook_raw[1:]:
+            temp_phonebook.append(name_fixer(*note[:3]))
+            temp_phonebook[-1].extend([note[3], note[4]])
+            temp_phonebook[-1].extend([phones_fixer(note[5])])
+            temp_phonebook[-1].extend([note[6]])
+
+        # избавляемся от дублей
+        for i, note1 in enumerate(temp_phonebook):
+            f = True
+            for j, note2 in enumerate(temp_phonebook[i+1:]):
+                if (note1[0], note1[1]) == (note2[0], note2[1]):
+                    temp_phonebook[j+i+1] = coupler(note1, note2)
+                    f = False
+                    break
+            if f: self.phonebook.append(note1)
+                    
+        return True
